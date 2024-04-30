3f2765dd51c78eb889fb3e1b843fa292b0bc29ab
Gianna Sharni <GiannaSharni@gmail.com>
2024-04-30 17:17:44 +0100

    completed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ApplicationRecord
-  def change
-    create_table :tasks do |t|
-      t.string :title
-      t.text :details
-      t.boolean :completed
-      
-      t.timestamps
-    end
 end
+
+  # def change
+  #   create_table :tasks do |t|
+  #     t.string :title
+  #     t.text :details
+  #     t.boolean :completed
+
+  #     t.timestamps
+  #   end
